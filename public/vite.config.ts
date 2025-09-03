d9594b876d14699c750a4169075e4ec169603bfc
Preston Pressoir <123640756+Pvpres@users.noreply.github.com>
2025-09-02 21:13:32 -0400

    Delete public/vite.config.ts

--- public/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Config for custom domain at root: https://paretojourneys.com
-export default defineConfig({
-  plugins: [react()],
-  base: "/",   // IMPORTANT: root of a custom domain
-});
