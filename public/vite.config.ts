6b4500c072dfa05155411042001a855bf5274ae9
Mattsaball <matthewcyntje@gmail.com>
2025-09-02 21:12:14 -0400

    Create vite.config.ts for React project in correct folder

---
+++ public/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Config for custom domain at root: https://paretojourneys.com
+export default defineConfig({
+  plugins: [react()],
+  base: "/",   // IMPORTANT: root of a custom domain
+});
